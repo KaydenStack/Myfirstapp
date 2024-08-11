00779b7b904670e657eaf506a13f130550bb62b7
KaydenStack <khanh.vu1024@gmail.com>
2024-08-11 14:13:58 +1000

    Add files via upload
    Add a random file to my first app

---
+++ Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Product
+    {
+        public int pid { get; set; }
+        public string product_name { get; set; }
+        public double price { get; set; }
+
+    }
+}
